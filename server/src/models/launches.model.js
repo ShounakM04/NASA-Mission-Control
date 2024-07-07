219f04177379bd5e8a6950080a8eb55bc2b0f3e3
Shounakmulay04 <interpidlynx@gmail.com>
2024-07-08 02:05:44 +0530

    done

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,6 +1,8 @@
-const launches = new Map();
+const launchesDatabase = require('./launches.mongo')
+const planets = require('./planets.mongo')
+// const launches = new Map();
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 0;
 
 const launch = {
   flightNumber: 100,
@@ -13,45 +15,70 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch)
+// launches.set(launch.flightNumber, launch);
 
-function existsLaunchWithId(launchId){
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId){
+  return await launchesDatabase.findOne({
+    flightNumber:launchId
+  });
 }
 
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getAllLaunches() {
+  return await launchesDatabase
+    .find({},{'_id':0, '__v':0})
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  // Reorder properties to ensure flightNumber is first
-  const newLaunch = {
-    flightNumber: latestFlightNumber,
-    mission: launch.mission,
-    rocket: launch.rocket,
-    launchDate: launch.launchDate,
-    target: launch.target,
-    customer: ["ZTM", "NASA"],
-    upcoming: true,
-    success: true,
-  };
-  launches.set(latestFlightNumber, newLaunch);
-}
+async function saveLaunch(launch){
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
 
-function abortLaunchWithId(launchId){
-  const aborted = launches.get(launchId);
-  if (aborted) {
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+  if(!planet){
+    throw new Error('No matching planet found')
   }
-  return null;
+  await launchesDatabase.updateOne({
+    flightNumber: launch.flightNumber,
+  },launch,{
+    upsert:true
+  })
+}
+
+async function scheduleNewLaunch(launch){
+  const newFlightNumber = await getLatestFlightNumber() + 1;
+  const newLaunch = Object.assign(launch,{
+    success:true,
+    upcoming: true,
+    customers: ['Zero to Mastery', 'NASA'],
+    flightNumber: newFlightNumber,
+  });
+  await saveLaunch(newLaunch)
+}
+
+async function getLatestFlightNumber(){
+  const latestLaunch = await launchesDatabase 
+    .findOne()
+    .sort('-flightNumber');
+
+  if(!latestLaunch){
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
+}
+
+async function abortLaunchWithId(launchId){
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId,
+  },{
+    upcoming:false,
+    success: false,
+  })
+  return aborted.modifiedCount === 1;
 }
 
 module.exports = {
   getAllLaunches,
-  addNewLaunch,
   existsLaunchWithId,
+  scheduleNewLaunch,
   abortLaunchWithId
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,25 @@
 const http = require('http');
-
+const mongoose = require('mongoose')
 const app = require('./app')
 
 const {loadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:WEIAmKdW6PmaQOxv@cluster0.6tvt2or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const server = http.createServer(app);
 
+mongoose.connection.on('open', ()=>{
+    console.log('mongo');
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err);
+})
+
 async function startServer(){
+    await mongoose.connect(MONGO_URL)
     await loadPlanetsData();
     server.listen(PORT, ()=>{
         console.log(`Listening on port ${PORT}`);
